73e0fa1fe98486e2b07f1818c2ca9f4ad8310f80
T1m0fe1 <timofei.magi@gmail.com>
2026-01-30 12:16:20 +0200

    Fixed menu deletion filter

--- inc/core/secure-mistakes.php
+++ inc/core/secure-mistakes.php
@@ -1,10 +1,16 @@
 <?php
 
-// Prevent menu deletion
-add_action('wp_delete_nav_menu', function ($term_id) {
+add_filter('pre_delete_term', function (int $term, string $taxonomy) {
     $strict_mode = get_field('security_strict_mode', 'option');
 
-    if ($strict_mode) {
+    if (!$strict_mode) {
+        return $term;
+    }
+
+    // Prevent menu deletion
+    if ($taxonomy === 'nav_menu') {
         wp_die(__('Menüü kustutamine on strict turbeolekus keelatud.', 'tondi'));
     }
-});
+
+    return $term;
+}, 10, 2);
