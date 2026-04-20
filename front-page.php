17e36153c0be154d4580bdb8cc9f39c3182e5cfe
T1m0fe1 <timofei.magi@gmail.com>
2026-04-20 13:01:57 +0300

    Updated news count and news styles

--- front-page.php
+++ front-page.php
@@ -28,7 +28,7 @@ get_header();
 
                 $news_q = new WP_Query([
                     'post_type' => 'news',
-                    'posts_per_page' => 3,
+                    'posts_per_page' => 6,
                     'post_status' => 'publish',
                     'orderby' => [
                         'menu_order' => 'ASC',
@@ -52,7 +52,7 @@ get_header();
                                     </a>
                                 </h2>
 
-                                <p class="news-card-excerpt">
+                                <!-- <p class="news-card-excerpt">
                                     <?php
                                     if (has_excerpt()) {
                                         echo esc_html(get_the_excerpt());
@@ -60,7 +60,7 @@ get_header();
                                         echo esc_html(wp_trim_words(wp_strip_all_tags(get_the_content('')), 24));
                                     }
                                     ?>
-                                </p>
+                                </p> -->
 
                                 <div class="news-card-image-wrapper">
                                     <?php
@@ -77,9 +77,9 @@ get_header();
                                     ?>
                                 </div>
 
-                                <a class="news-card-more" href="<?php the_permalink(); ?>">
+                                <!-- <a class="news-card-more" href="<?php the_permalink(); ?>">
                                     <?php esc_html_e('Loe edasi...', 'tondi'); ?>
-                                </a>
+                                </a> -->
                             </li>
                         <?php endwhile;
                         wp_reset_postdata(); ?>
